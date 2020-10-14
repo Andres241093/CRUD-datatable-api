bc49e223a88127f84f321c5044a45d4fddf6f1aa
Andres <andresucero241093@gmail.com>
2020-10-14 01:34:34 -0430

    fisrt commit

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->method()) {
+            case 'GET':
+            {
+                return [];
+            }
+            break;
+            case 'POST':
+            {
+                return [
+                   'code'=>'required|unique:articles',
+                   'price'=>'min:1',
+                   'description'=>'min:5'
+               ];
+            }
+            break;
+            case 'PUT':
+            {
+                return [
+                   'code'=>'required|unique:articles',
+                   'price'=>'min:1',
+                   'description'=>'min:5'
+                ];
+            }
+            break;
+            case 'DELETE': break;
+            case 'PATCH': break;
+            default:
+            {
+                return [];
+            }
+            break;
+        }
+    }
+
+    public function attributes()
+    {
+      return [
+            'code' => 'código'  
+      ];
+    }
+}
